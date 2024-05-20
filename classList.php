5e25be185fce6da585124186c7d98a7b878bd9d9
TuanNguyenas <165824612+TuanNguyenas@users.noreply.github.com>
2024-05-20 20:46:12 +0700

    classList.php

--- classList.php
+++ classList.php
@@ -29,8 +29,8 @@
             </div>
 
             <div class="menu-bar">
-                <div class="first-option"><a href="">trang chủ</a></div>
-                <div class="second-option"><a href="" >dịch vụ của tôi</a></div>
+                <div class="first-option"><a href="homeAfterLogin_Teacher.php">trang chủ</a></div>
+                <div class="second-option"><a href="homeAfterLogin_Teacher.php" >dịch vụ của tôi</a></div>
             </div>
         </div>
         
